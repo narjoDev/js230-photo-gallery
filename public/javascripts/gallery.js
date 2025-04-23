66b7a530ea377ff389f62067b1de82aeb6b256c9
narjoDev <63086420+narjoDev@users.noreply.github.com>
2025-04-23 15:42:47 -0700

    fetch photos, render photos and info

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const slideshow = document.getElementById("slideshow");
   const slides = document.getElementById("slides");
+  const info = document.querySelector("section > header");
   const comments = document.getElementById("comments");
+
+  let photos;
 
   const templateFromId = (id) =>
     Handlebars.compile(document.getElementById(id).innerHTML);
@@ -16,4 +19,37 @@ document.addEventListener("DOMContentLoaded", () => {
     "photo_comment",
     document.getElementById("photo_comment").innerHTML
   );
+
+  getPhotos()
+    .then(renderPhotos)
+    .then(() => renderInfo(0));
+
+  async function getPhotos() {
+    return await get("/photos").then((photosJson) => {
+      photos = photosJson;
+    });
+  }
+
+  function renderPhotos() {
+    slides.innerHTML = templates.photos({ photos });
+  }
+
+  function renderInfo(index) {
+    info.innerHTML = templates.information(photos[index]);
+  }
+
+  function get(url) {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.open("GET", url);
+      request.responseType = "json";
+
+      request.addEventListener("load", (event) => {
+        resolve(request.response);
+      });
+      request.addEventListener("error", reject);
+
+      request.send();
+    });
+  }
 });
