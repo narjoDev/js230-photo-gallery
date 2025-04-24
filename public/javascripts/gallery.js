2d3e23952305f2e8d8aba3bd25c498fdeead54a7
narjoDev <63086420+narjoDev@users.noreply.github.com>
2025-04-24 11:26:28 -0700

    implement slideshow without animation

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -16,12 +16,16 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 
   let photos;
+  let activeIndex = 0;
 
   getPhotos()
     .then(renderPhotos)
     .then(() => renderInfo(0))
     .then(() => getComments(photos[0].id))
     .then(renderComments);
+
+  document.querySelector(".prev").addEventListener("click", previous);
+  document.querySelector(".next").addEventListener("click", next);
 
   async function getPhotos() {
     return await get("/photos").then((photosJson) => {
@@ -62,5 +66,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function templateFromId(id) {
     return Handlebars.compile(document.getElementById(id).innerHTML);
+  }
+
+  function previous(event) {
+    event.preventDefault();
+    changeSlide(-1);
+  }
+
+  function next(event) {
+    event.preventDefault();
+    changeSlide(1);
+  }
+
+  function changeSlide(offset) {
+    const oldActive = slides.children[activeIndex];
+    activeIndex = (photos.length + activeIndex + offset) % photos.length;
+    const newActive = slides.children[activeIndex];
+    const newId = newActive.dataset.id;
+
+    oldActive.classList.remove("active");
+    oldActive.classList.add("inactive");
+    newActive.classList.remove("inactive");
+    newActive.classList.add("active");
+    renderInfo(activeIndex);
+    getComments(newId).then(renderComments);
   }
 });
